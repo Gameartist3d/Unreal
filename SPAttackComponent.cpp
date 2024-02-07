eac651450e3f2f1905c5c72b32bccaf9c61f7295
John <47099626+Gameartist3d@users.noreply.github.com>
2024-02-07 00:56:18 -0500

    Create SPAttackComponent.cpp

---
+++ SPAttackComponent.cpp
@@ -0,0 +1,20 @@
+#include "SPAttackComponent.h"
+
+// Sets default values for this component's properties
+USPAttackComponent::USPAttackComponent()
+{
+	
+	
+
+	// ...
+}
+
+
+// Called when the game starts
+void USPAttackComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
