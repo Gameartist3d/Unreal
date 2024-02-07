4bb4e79fd37b3f7cb7aae21da96c09a3f8f8655a
John <47099626+Gameartist3d@users.noreply.github.com>
2024-02-07 00:14:53 -0500

    Update FHealthVariables.cpp

--- FHealthVariables.cpp
+++ FHealthVariables.cpp
@@ -1,11 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "FHealthVariables.h"
+
 
 FHealthVariables::FHealthVariables()
 {
-	HealthVariableName = FText::FromString(TEXT("Body Part"));
 	MaxHealth = 100.0f;
 	CurHealth = 100.0f;
 	DamHealth = 0.0f;
