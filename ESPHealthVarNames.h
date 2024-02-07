b9ef923cfe86380f18a831435eba879d24b4675e
John <47099626+Gameartist3d@users.noreply.github.com>
2024-02-07 00:17:14 -0500

    Create ESPHealthVarNames.h

---
+++ ESPHealthVarNames.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ESPHealthVarNames.generated.h"
+
+UENUM(BlueprintType)
+enum class ESPHealthVarNames : uint8
+{
+	Head,
+	Torso,
+	Arm,
+	Leg,
+	Tail,
+	Wing,
+	Horn,
+	Hoove,
+	Shell,
+	Carapace,
+	Stomach,
+	Eye,
+	Jaw,
+	Nose,
+	Ear,
+	Knee,
+	Elbow,
+	AssCheek
+
+};
