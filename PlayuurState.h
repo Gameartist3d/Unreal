0f0b7339b0e3f188afc7fe06c226dcf342c64b0b
John <47099626+Gameartist3d@users.noreply.github.com>
2024-02-07 00:56:45 -0500

    Create PlayuurState.h

---
+++ PlayuurState.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "PlayuurState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SP_API APlayuurState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
