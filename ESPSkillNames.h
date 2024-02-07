ffd7d1d99823eb2805c796da029225c3928b992f
John <47099626+Gameartist3d@users.noreply.github.com>
2024-02-07 00:15:44 -0500

    Create ESPSkillNames.h

---
+++ ESPSkillNames.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ESPSkillNames.generated.h"
+
+/**
+ * 
+ */
+UENUM(BlueprintType)
+enum class ESPSkillNames : uint8
+{
+	MartialCombat,
+	OneHandMartialWeapon,
+	TwoHandMartialWeapon,
+	OneHandProjectileWeapon,
+	TwoHandProjectileWeapon,
+	Climbing,
+	Vaulting, 
+	Sprinting,
+	Swimming,
+	Bartering,
+	Hacking,
+	Manipulation,
+	Channeling,
+	Throwing,
+	Dodging,
+	Invention,
+	Manufactor,
+	Investigation,
+	Conjuration,
+	Stealth,
+	Alchemy,
+	Medical,
+	Balance,
+	Piloting
+
+};
