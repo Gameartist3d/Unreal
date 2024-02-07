89951a06134e5a270b2482221d00b7c6069d0583
John <47099626+Gameartist3d@users.noreply.github.com>
2024-02-07 00:16:27 -0500

    Create FSkill.h

---
+++ FSkill.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ESPSkillNames.h"
+#include "FSkill.generated.h"
+
+//Struct for Skill Variables
+
+USTRUCT(BlueprintType)
+struct SP_API FSkill
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Skill")
+	ESPSkillNames SkillName;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Skill")
+	class UTexture2D* SkillIcon;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Skill")
+	int32 SkillLvl;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Skill")
+	float SkillExp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Skills")
+	TArray<ESPDisciplineNames> ParentDisciplines;
+};
