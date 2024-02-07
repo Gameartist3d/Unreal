cabee7bf341e3b1337380c521cd8591587908415
John <47099626+Gameartist3d@users.noreply.github.com>
2024-02-07 00:34:28 -0500

    Create ESPCurrency.h

---
+++ ESPCurrency.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ESPCurrency.generated.h"
+
+/**
+ * 
+ */
+UENUM(BlueprintType)
+enum class ESPCurrency : uint8
+{
+	Rupee,
+	GrotanShekel,
+	GalacticShilling,
+	Medaos,
+	Aureus
+
+};
