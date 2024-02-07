38186ec11a6bde640da106df9ac6a519e8facb0f
John <47099626+Gameartist3d@users.noreply.github.com>
2024-02-07 00:35:37 -0500

    Create EAttributeName.h

---
+++ EAttributeName.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EAttributeName.generated.h"
+
+/**
+ * 
+ */
+UENUM(BlueprintType)
+enum class EAttributeName : uint8
+{
+	Physical,
+	Mental
+};
