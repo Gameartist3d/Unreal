8b206f78280821b148694a3338e6a0ac55c0d617
John <47099626+Gameartist3d@users.noreply.github.com>
2024-02-07 00:31:59 -0500

    Create ESPDisciplineNames.h

---
+++ ESPDisciplineNames.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ESPDisciplineNames.generated.h"
+
+/**
+ * 
+ */
+UENUM(BlueprintType)
+enum class ESPDisciplineNames : uint8
+{
+	Strength,
+	Endurance,
+	Flexibility,
+	Reflex,
+	Technical,
+	Mechanical,
+	Social,
+	Spiritual
+};
