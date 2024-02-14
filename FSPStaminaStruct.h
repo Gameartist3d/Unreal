6146390b8a5b8c113ea76f3062a42ff8107a1e95
John <47099626+Gameartist3d@users.noreply.github.com>
2024-02-13 20:28:09 -0500

    Update FSPStaminaStruct.h
    Added new variable, changed comment

--- FSPStaminaStruct.h
+++ FSPStaminaStruct.h
@@ -1,3 +1,5 @@
+// 3DNomad LLC
+
 #pragma once
 
 #include "CoreMinimal.h"
@@ -11,12 +13,19 @@ struct FSPStaminaStruct
 {
 	GENERATED_BODY()
 
+	//Maximum Stamina
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stamina")
 	float MaxStamina;
 
+	//Current amount of stamina
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stamina")
 	float CurStamina;
 
+	//Damaged stamina, ie. unable to regenerate
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stamina")
 	float DamStamina;
+
+	//Amount of Stamina to recover
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stamina")
+	float RecStamina;
 };
