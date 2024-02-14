6a7a0f3c613c0bc7d531cd1d5a645bb81868bf3e
John <47099626+Gameartist3d@users.noreply.github.com>
2024-02-13 20:36:37 -0500

    Create FMagickaStruct.cpp

---
+++ FMagickaStruct.cpp
@@ -0,0 +1,13 @@
+//3DNomad LLC
+#include "FMagickaStruct.h"
+
+FMagickaStruct::FMagickaStruct()
+{
+	MaxMagic = 50.0f;
+	CurMagic = 50.0f;
+	MagicFatigue = 0.0f;
+	ChannelSpeed = 1.0f;
+	SpellStackAmount = 1;
+	DamMagic = 0.0f;
+	RecMagic = 0.0f;
+}
