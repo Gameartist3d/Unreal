7565d2aaf8e6f6548abfc82fca9c48e0428d987f
John <47099626+Gameartist3d@users.noreply.github.com>
2024-02-13 20:38:18 -0500

    Update FHealthVariables.h
    added bools and comment change

--- FHealthVariables.h
+++ FHealthVariables.h
@@ -1,3 +1,5 @@
+//3DNomad LLC
+
 #pragma once
 
 #include "CoreMinimal.h"
@@ -10,24 +12,37 @@ struct FHealthVariables
 {
     GENERATED_BODY()
 
+    //Health variable name
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Health")
     ESPHealthVarNames HealthVariableName;
 
+    //Icon used for UI
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Health")
     class UTexture2D* HVariableIcon;
 
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Health")
+    bool isLeftAppendage;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Health")
+    bool isRightAppendage;
+
+    //maximum health
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Health")
     float MaxHealth;
 
+    //current health amount
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Health")
     float CurHealth;
 
+    //damaged health
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Health")
     float DamHealth;
 
+    //health to recover
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Health")
     float RecHealth;
 
+    //health that is poisoned
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Health")
     float PoisHealth;
 
