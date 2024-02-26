9f5f887070c97a2eff2539c518f5a2106e6f8e0a
John <47099626+Gameartist3d@users.noreply.github.com>
2024-02-26 01:07:20 -0500

    Update BaseItem.cpp
    added enum intialization

--- BaseItem.cpp
+++ BaseItem.cpp
@@ -1,9 +1,15 @@
+//3DNomad LLC
+
+
 #include "BaseItem.h"
 
 UBaseItem::UBaseItem()
 {
 	UseActionText = FText::FromString(TEXT("Use"));
 	ItemDisplayName = FText::FromString(TEXT("Item"));
+	ItemType = ESPItemType::Program;
+	ItemRarity = ESPItemRarity::Common;
 	ItemDescription = FText::FromString(TEXT("Base Item"));
 	ItemWeight = 1.0f;
 }
+
