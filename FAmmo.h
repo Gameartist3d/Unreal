5fb737c911bb1828ead1919a95644f73a150eeb5
John <47099626+Gameartist3d@users.noreply.github.com>
2024-03-04 02:03:09 -0500

    Update FAmmo.h
    added enum for names

--- FAmmo.h
+++ FAmmo.h
@@ -9,15 +9,38 @@
 /**
  * 
  */
+UENUM(BlueprintType)
+enum class ESPAmmoName : uint8
+{
+	B7G,
+	B12G,
+	B15G,
+	B20G,
+	B35G,
+	B55G,
+	B60G,
+	B70G,
+	B75G,
+	WarpShot2G,
+	WarpShot6G,
+	HeatSink5G,
+	HeatSink10G,
+	Rtip10G,
+	Rtip15G
+};
 USTRUCT(BlueprintType)
 struct FAmmo
 {
 	GENERATED_BODY()
 
+	FAmmo();
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Ammo")
-	FText AmmoName;
+	ESPAmmoName AmmoName;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Ammo")
 	TArray<FSPDamageType> AmmoDamageTypes;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Ammo")
+	int32 AmmoAmount;
 };
