171aec40877ad2bc51610c5fefecc8b69b72677a
John <47099626+Gameartist3d@users.noreply.github.com>
2024-03-25 02:08:09 -0400

    Update FThrowable.h
    added types

--- FThrowable.h
+++ FThrowable.h
@@ -1,6 +1,9 @@
+//3DNomad LLC
+
 #pragma once
 
 #include "CoreMinimal.h"
+#include "ESPDamageType.h"
 #include "FThrowable.generated.h"
 
 /**
@@ -11,8 +14,15 @@ struct FThrowable
 {
 	GENERATED_BODY()
 
+	FThrowable();
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Throwables")
 	FText ThrowableName;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Throwables")
+	TArray<class FSPDamageType> ThrowableDamageTypes;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Throwables")
+	float ThrowablesWeight;
 
 };
