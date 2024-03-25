aea6324509625672b0ab626cc1d54e1a877427be
John <47099626+Gameartist3d@users.noreply.github.com>
2024-03-25 02:02:54 -0400

    Create KnowledgeTopic.cpp

---
+++ KnowledgeTopic.cpp
@@ -0,0 +1,14 @@
+//3DNomad LLC
+
+
+#include "KnowledgeTopic.h"
+
+KnowledgeTopic::KnowledgeTopic()
+{
+	TopicName = "Default";
+	TopicDescription = "Default Description";
+}
+
+KnowledgeTopic::~KnowledgeTopic()
+{
+}
