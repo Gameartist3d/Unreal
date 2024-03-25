86627a6447d7357234bb933a77b8a359f3c7e654
John <47099626+Gameartist3d@users.noreply.github.com>
2024-03-25 01:57:37 -0400

    Create EEmotionName.h

---
+++ EEmotionName.h
@@ -0,0 +1,29 @@
+//3DNomad LLc
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EEmotionName.generated.h"
+
+UENUM(BlueprintType)
+enum class EEmotionName : uint8
+{
+	Ecstasy,
+	Rage,
+	Grief,
+	Terror,
+	Joy,
+	Fear,
+	Sadness,
+	Disgust,
+	Anger,
+	Anticipation,
+	Love,
+	Submission,
+	Awe,
+	Disapproval,
+	Remorse,
+	Contempt,
+	Aggressiveness,
+	Optimism
+};
