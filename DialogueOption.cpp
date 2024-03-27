c27bd27e8b276610b909b570bfa3de69ca68abcb
John <47099626+Gameartist3d@users.noreply.github.com>
2024-03-26 20:21:03 -0400

    Create DialogueOption.cpp

---
+++ DialogueOption.cpp
@@ -0,0 +1,13 @@
+//3DNomad LLC
+
+
+#include "DialogueOption.h"
+
+UDialogueOption::UDialogueOption()
+{
+	DialogueCategory = EDialogueCategory::General;
+}
+
+UDialogueOption::~UDialogueOption()
+{
+}
