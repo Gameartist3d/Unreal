4c0b4b13c13fef8a40fcdb3cad8ae7f59cc32dac
John <47099626+Gameartist3d@users.noreply.github.com>
2024-03-26 20:12:33 -0400

    Update NPCSocialComponent.h
    migrated to Usocialcomponent

--- NPCSocialComponent.h
+++ NPCSocialComponent.h
@@ -5,12 +5,12 @@
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "SPCharacter.h"
-#include "KnowledgeTopic.h"
+#include "USocialComponent.h"
 #include "NPCSocialComponent.generated.h"
 
-//TODO make class inherit from DEFAULT social component class
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class SP_API UNPCSocialComponent : public UActorComponent
+class SP_API UNPCSocialComponent : public UUSocialComponent
 {
 	GENERATED_BODY()
 
@@ -21,15 +21,9 @@ public:
 	UPROPERTY(EditAnywhere, Category = "Playuur")
 	ASPCharacter* TalkingPlayuur;
 
-	UPROPERTY(EditAnywhere, Category = "Dialogue")
-	TArray<class AKnowledgeTopic*> DialogueTopics;
-
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
-	void AddDialogueTopic(class AKnowledgeTopic* Topic);
-	void RemoveDialogueTopic(class AKnowledgeTopic* Topic);
 
 public:	
 	// Called every frame
