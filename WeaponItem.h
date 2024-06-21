27933de87d4abb887b50a6dbfd6d3abae10664ad
John <47099626+Gameartist3d@users.noreply.github.com>
2024-06-21 01:23:02 -0400

    Update WeaponItem.h
    Added functionality

--- WeaponItem.h
+++ WeaponItem.h
@@ -1,25 +1,61 @@
+//3DNomad LLC
+
 #pragma once
 
 #include "CoreMinimal.h"
-#include "BaseItem.h"
+#include "SPBaseItem.h"
 #include "FWeaponStats.h"
+#include "FSkill.h"
 #include "WeaponItem.generated.h"
 
 /**
  * 
  */
 
-UCLASS(Abstract, BlueprintType, Blueprintable, EditInLineNew, DefaultToInstanced)
-class SP_API UWeaponItem : public UBaseItem
+UCLASS(BlueprintType, Blueprintable, DefaultToInstanced)
+class SP_API AWeaponItem: public ASPBaseItem
 {
 	GENERATED_BODY()
 
+	float CalculateTotalDamage();
+
 public:
 
-	UWeaponItem();
+	AWeaponItem();
 
+	//Weapon Stats Struct
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
 	FWeaponStats WeaponStats;
+
+	//Weapon Skill Bonuses
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	TMap<ESPSkillNames, float> SkillBonuses;
+
+	//Weapon Aggro Amount
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	float AggroAmount;
+
+	//bool to check if weapon is two handed
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	bool bIsTwoHanded;
+
+	//Physics force applied to the hit actor/object
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	float PhysicsForce;
+
+	//Skeletal Mesh for the weapon
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	USkeletalMesh* WeaponMesh;
+
+	//Function for comparing damage types againsy health variable resistances, and then applying the residual damage
+	UFUNCTION(BlueprintCallable)
+	void ApplyDamageTypes(FHitResult hitresult);
+
+	//Function for applying the weapon's physics force to the hit actor bone
+	UFUNCTION(BlueprintCallable)
+	void ApplyPhysicsForce(FHitResult hitresult, ASPCharacter* Character);
+	
+
 
 
 protected:
