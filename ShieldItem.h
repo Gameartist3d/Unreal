f45ccaf1b69549279f2bbcf300aef1bd5fafd2ea
John <47099626+Gameartist3d@users.noreply.github.com>
2024-06-21 01:38:17 -0400

    Update ShieldItem.h
    added shield variables

--- ShieldItem.h
+++ ShieldItem.h
@@ -1,21 +1,80 @@
+//3DNomad LLC
+
 #pragma once
 
 #include "CoreMinimal.h"
-#include "BaseItem.h"
+#include "SPBaseItem.h"
+#include "ESPDamageType.h"
 #include "ShieldItem.generated.h"
 
 /**
  * 
  */
-UCLASS(Abstract, BlueprintType, Blueprintable, EditInLineNew, DefaultToInstanced)
-class SP_API UShieldItem : public UBaseItem
+UENUM(BlueprintType)
+enum class EShieldColour : uint8
+{
+	None,
+	White,
+	Green,
+	Blue,
+	Teal,
+	Orange,
+	Purple,
+	Yellow,
+	Red,
+	Peach,
+	Brown,
+	Grey,
+	LightBlue,
+	Pink,
+	Rainbow,
+	Black
+};
+
+UCLASS(Abstract, BlueprintType, Blueprintable, DefaultToInstanced)
+class SP_API AShieldItem : public ASPBaseItem
 {
 	GENERATED_BODY()
 
 public:
 
-	UShieldItem();
+	AShieldItem();
 
+	//Shield Strength
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	float ShieldStrength;
+
+	//Shield Current Strength
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	float ShieldCurrentStrength;
+
+	//Shield Regen Rate
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	float ShieldRegenRate;
+
+	//float for shield hacked amount
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	float ShieldHackedAmount;
+
+	//Shield Regen Delay
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	float ShieldRegenDelay;
+
+	//Shield Resistances
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resistances")
+	TMap<ESPDamageType, float> ShieldResistances;
+
+	//Shield Colour
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	EShieldColour ShieldColour;
+
+	//bool to check if shield has broken
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	bool bShieldBroken = false;
+	
+	//Function to update bshieldbroken if shield is broken
+	UFUNCTION(BlueprintCallable, Category = "Shield")
+	void UpdateShieldBroken();
 
 
 protected:
