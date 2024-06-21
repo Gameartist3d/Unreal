f34d7a9a5d31e20fc2d65e733d8b5f9738def1e6
John <47099626+Gameartist3d@users.noreply.github.com>
2024-06-21 01:51:04 -0400

    Update ArmorItem.h
    added variables

--- ArmorItem.h
+++ ArmorItem.h
@@ -1,20 +1,57 @@
+//3DNomad LLC
+
 #pragma once
 
 #include "CoreMinimal.h"
-#include "BaseItem.h"
+#include "SPBaseItem.h"
+#include "ESPHealthVarNames.h"
+#include "ESPDamageType.h"
 #include "ArmorItem.generated.h"
 
 /**
  * 
  */
-UCLASS(Abstract, BlueprintType, Blueprintable, EditInLineNew, DefaultToInstanced)
-class SP_API UArmorItem : public UBaseItem
+UENUM(BlueprintType)
+enum class EArmorColour : uint8
+{
+	None,
+	White,
+	Green,
+	Blue,
+	Teal,
+	Orange,
+	Purple,
+	Yellow,
+	Red,
+	Peach,
+	Brown,
+	Grey,
+	LightBlue,
+	Pink,
+	Rainbow,
+	Black
+};
+
+UCLASS(BlueprintType, Blueprintable, DefaultToInstanced)
+class SP_API AArmorItem : public ASPBaseItem
 {
 	GENERATED_BODY()
 
 public:
 
-	UArmorItem();
+	AArmorItem();
+
+	//Damage Resistance
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resistances")
+	TMap<ESPDamageType, float> ArmorResistances;
+
+	//Health variable names that will be affected by the armor
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+	TArray<ESPHealthVarNames> AffectedHealthVariables;
+
+	//Armor Colour
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Colour")
+	EArmorColour Colour;
 
 protected:
 
