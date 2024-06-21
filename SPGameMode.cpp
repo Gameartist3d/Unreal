e7d1ba8d30f33fe9c745da7260d1596261df8769
John <47099626+Gameartist3d@users.noreply.github.com>
2024-06-21 01:34:14 -0400

    Create SPGameMode.cpp

---
+++ SPGameMode.cpp
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SPGameMode.h"
+#include "SPCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ASPGameMode::ASPGameMode()
+{
+}
+
+void ASPGameMode::PawnKilled(APawn* killedpawn)
+{
+}
