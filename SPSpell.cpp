349d366238fcc2dd5fd79e5b81f5a6a7a4fa689a
John <47099626+Gameartist3d@users.noreply.github.com>
2024-06-21 01:19:20 -0400

    Create SPSpell.cpp

---
+++ SPSpell.cpp
@@ -0,0 +1,43 @@
+//3DNomad LLC
+
+
+#include "SPSpell.h"
+
+USPSpell::USPSpell()
+{
+		Name = SpellNames::Allure;
+		Icon = nullptr;
+		Description = "Allure is a spell that makes the caster more attractive to others.";
+		Difficulty = 1.0f;
+		ManaCost = 10.0f;
+		Value = 10.0f;
+		TechLevel = 1;
+		SpiritLevel = 1;
+		EnduranceLevel = 1;
+		MechanicalLevel = 1;
+		SocialLevel = 1;
+}
+
+void USPSpell::CastSpell()
+{
+
+}
+
+void USPSpell::UpgradeSpell()
+{
+
+}
+
+void USPSpell::DowngradeSpell()
+{
+}
+
+void USPSpell::DestroySpell()
+{
+	
+}
+
+void USPSpell::CombineSpell()
+{
+
+}
