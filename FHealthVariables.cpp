7f931c4c0cc0728168aadd030733eff4800f25fe
John <47099626+Gameartist3d@users.noreply.github.com>
2024-06-21 01:48:58 -0400

    Update FHealthVariables.cpp
    only destructor left TODO add destroying functionality if need be

--- FHealthVariables.cpp
+++ FHealthVariables.cpp
@@ -2,23 +2,6 @@
 
 
 #include "FHealthVariables.h"
-
-
-FHealthVariables::FHealthVariables()
-{
-	HealthVariableName = ESPHealthVarNames::AssCheek;
-	isLeftAppendage = false;
-	isRightAppendage = false;
-	HVariableIcon = nullptr;
-	MaxHealth = 100.0f;
-	CurHealth = 100.0f;
-	DamHealth = 0.0f;
-	RecHealth = 0.0f;
-	PoisHealth = 0.0f;
-	HeatAmount = 0.0f;
-	ColdAmount = 0.0f;
-}
-
 FHealthVariables::~FHealthVariables()
 {
 }
