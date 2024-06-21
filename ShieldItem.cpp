18d3321701e105d0be39c88de01b524ff8f40a88
John <47099626+Gameartist3d@users.noreply.github.com>
2024-06-21 01:39:10 -0400

    Update ShieldItem.cpp
    Added variables and update functionality

--- ShieldItem.cpp
+++ ShieldItem.cpp
@@ -5,14 +5,34 @@
 #include "SPCharacter.h"
 
 
-UShieldItem::UShieldItem()
+AShieldItem::AShieldItem()
 {
 	ItemDisplayName = FText::FromString(TEXT("Shield"));
-	UseActionText = FText::FromString(TEXT("Equip"));
 	ItemType = ESPItemType::Shield;
+	ItemSlots.Add(ESPEquipmentSlot::Shield);
+	ShieldRegenDelay = 5.0f;
+	ShieldRegenRate = 5.0f;
+	ShieldStrength = 10.0f;
+	ShieldCurrentStrength = 10.0f;
+	ShieldHackedAmount = 0.0f;
+	ShieldColour = EShieldColour::None;
+	InteractionComponent->InteractionTypes.Add(EInteractionTypes::Pickup);
+	InteractionComponent->InteractionTypes.Add(EInteractionTypes::Equip);
+	InteractionComponent->InteractionTypes.Add(EInteractionTypes::Examine);
+	InteractionComponent->InteractionTypes.Add(EInteractionTypes::Compare);
 }
 
-void UShieldItem::Use(ASPCharacter* Character)
+void AShieldItem::UpdateShieldBroken()
+{
+	if (ShieldCurrentStrength <= 0.0f) {
+		bShieldBroken = true;
+	}
+	else {
+		bShieldBroken = false;
+	}
+}
+
+void AShieldItem::Use(ASPCharacter* Character)
 {
 	
 }
