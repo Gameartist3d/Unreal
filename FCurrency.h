1985115a432d87d6b8e82948a5b8439309f4eb34
John <47099626+Gameartist3d@users.noreply.github.com>
2025-01-22 00:40:27 -0500

    Update FCurrency.h

--- FCurrency.h
+++ FCurrency.h
@@ -1,28 +1,78 @@
-// 3DNomad LLC
+//3DNomad LLC
 
 #pragma once
 
 #include "CoreMinimal.h"
-#include "ESPCurrency.h"
+#include "UObject/NoExportTypes.h"
 #include "FCurrency.generated.h"
 
-/**Struct for creating new Currency types
+/**
+ * 
  */
+
+ //Enum for Currency names
+UENUM(BlueprintType)
+enum class ECurrencyName : uint8
+{
+	Rupee UMETA(DisplayName = "Rupee"),
+	GrotanShekel UMETA(DisplayName = "Grotan Shekel"),
+	GalacticShilling UMETA(DisplayName = "Galactic Shilling"),
+	Medaos UMETA(DisplayName = "Medaos"),
+	Aureus UMETA(DisplayName = "Aureus"),
+	SeaShells UMETA(DisplayName = "Sea Shells"),
+	Dollas UMETA(DisplayName = "Dollas"),
+	Coppers UMETA(DisplayName = "Coppers"),
+	BlackSouls UMETA(DisplayName = "Black Souls")
+};
+
 USTRUCT(BlueprintType)
 struct FCurrency
 {
-	GENERATED_BODY()
+GENERATED_BODY()
 
-public:
+UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Attribute")
+ECurrencyName CurrencyName;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Currency")
-	ESPCurrency CurrencyName;
+UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Attribute")
+UTexture2D* CurrencyIcon;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Currency")
-	class UTexture2D* CurrencyIcon;
+UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category = "Attribute")
+FText CurrencyDescription;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Currency")
-	float TotalInWallet;
+FCurrency()
+	: CurrencyName(ECurrencyName::Medaos),
+	CurrencyIcon(nullptr),
+	CurrencyDescription(FText::FromString(TEXT("Currency Description")))
+{
+}
+FCurrency(ECurrencyName inname, UTexture2D* inicon, FText indescription)
+	: CurrencyName(inname),
+	CurrencyIcon(inicon),
+	CurrencyDescription(indescription)
+{
+}
+FCurrency(const FCurrency& other)
+	: FCurrency(other.CurrencyName, other.CurrencyIcon, other.CurrencyDescription)
+{
+}
+bool operator==(const FCurrency& other) const
+{
+	return Equals(other);
+}
 
-	FCurrency();
+bool Equals(const FCurrency& other) const
+{
+	return CurrencyName == other.CurrencyName && CurrencyIcon == other.CurrencyIcon && CurrencyDescription.EqualTo(other.CurrencyDescription);
+}
 };
+
+#if UE_BUILD_DEBUG
+uint32 GetTypeHash(const FCurrency& thing);
+#else 
+FORCEINLINE uint32 GetTypeHash(const FCurrency& thing)
+{
+	uint32 hash = FCrc::MemCrc32(&thing, sizeof(FCurrency));
+	return hash;
+}
+#endif
+
