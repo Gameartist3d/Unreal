6ecb408ab5c9a08185be3e9604465a5151e948e3
John <47099626+Gameartist3d@users.noreply.github.com>
2025-01-22 00:25:16 -0500

    Create SPDefaultPlayerState.h

---
+++ SPDefaultPlayerState.h
@@ -0,0 +1,17 @@
+//3DNomad LLC
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "SPDefaultPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPV1_API ASPDefaultPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
