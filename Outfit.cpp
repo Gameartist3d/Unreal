422c0b8e6c9e9a1f2e2c6cb70e9a0a6334dce9ce
John <47099626+Gameartist3d@users.noreply.github.com>
2025-01-22 00:54:14 -0500

    Create Outfit.cpp

---
+++ Outfit.cpp
@@ -0,0 +1,27 @@
+//3DNomad LLC
+
+
+#include "Outfit.h"
+
+// Sets default values
+AOutfit::AOutfit()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AOutfit::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AOutfit::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
