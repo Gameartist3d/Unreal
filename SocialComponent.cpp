91f695013bce06f919226d91fc598d5e426669a7
John <47099626+Gameartist3d@users.noreply.github.com>
2025-01-22 00:23:50 -0500

    Update and rename USocialComponent.cpp to SocialComponent.cpp
    updated to project 5.5

---
+++ SocialComponent.cpp
@@ -0,0 +1,33 @@
+//3DNomad LLC
+
+
+#include "SocialComponent.h"
+
+// Sets default values for this component's properties
+USocialComponent::USocialComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void USocialComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void USocialComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
