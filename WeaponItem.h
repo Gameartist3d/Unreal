d74c4dc40738d351fc4b1bb87de4aece18d11e38
John <47099626+Gameartist3d@users.noreply.github.com>
2025-01-22 00:49:02 -0500

    Update WeaponItem.h

--- WeaponItem.h
+++ WeaponItem.h
@@ -3,63 +3,126 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "SPBaseItem.h"
-#include "FWeaponStats.h"
-#include "FSkill.h"
+#include "SPItem.h"
+#include "FDamage.h"
 #include "WeaponItem.generated.h"
 
 /**
  * 
  */
 
-UCLASS(BlueprintType, Blueprintable, DefaultToInstanced)
-class SP_API AWeaponItem: public ASPBaseItem
+UENUM(BlueprintType)
+enum class EWeaponMaterial : uint8
+{
+	Iron,
+	Steel,
+	Palladium,
+	Tungsten,
+	Gold,
+	Silver,
+	Mithril,
+	Adamantium,
+	Platinum,
+	Aluminum,
+	Crystal,
+	LivingMetal,
+	DemonicAmber,
+	LightThread,
+	TimeGlass,
+	Particle,
+	Fusion,
+	StarFire,
+	SaltyTears
+};
+
+UENUM(BlueprintType)
+enum class EWeaponCategory : uint8
+{
+	Dagger,
+	Knife,
+	Blade,
+	Sword,
+	Rapier,
+	Saber,
+	Pistol,
+	Handgun,
+	Revolver,
+	Rifle,
+	Gun,
+	Repeater,
+	Railgun,
+	Bow,
+	Crossbow,
+	Launcher,
+	Shotgun,
+	Lasergun,
+	Protongun,
+	Spear,
+	Lance,
+	Boomerang,
+	Stave
+};
+
+UCLASS(BlueprintType)
+class SPV1_API UWeaponItem : public USPItem
 {
 	GENERATED_BODY()
 
-	float CalculateTotalDamage();
-
 public:
 
-	AWeaponItem();
+	
+	/*Variables*/
+	 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	EWeaponCategory WeaponCategory;
 
-	//Weapon Stats Struct
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
-	FWeaponStats WeaponStats;
+	//Tmap of damage types and their values
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	TMap<EDamageType, float> WeaponDamageTypes;
 
-	//Weapon Skill Bonuses
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
-	TMap<ESPSkillNames, float> SkillBonuses;
+		//TODO Create Weapon Ability Class
+		/*UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+		TArray<UWeaponAbility*> WeaponAbilities;*/
 
-	//Weapon Aggro Amount
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
-	float AggroAmount;
+	//Weapon Material for further defining the weapon
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	EWeaponMaterial WeaponMaterial = EWeaponMaterial::SaltyTears;
+
+	//float for the aggro this weapon generates
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	float AggroAmount = 1.0f;
 
 	//bool to check if weapon is two handed
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
-	bool bIsTwoHanded;
+	bool bIsTwoHanded = false;
 
 	//Physics force applied to the hit actor/object
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
-	float PhysicsForce;
+	float PhysicsForce = 1.0f;
 
-	//Skeletal Mesh for the weapon
+	//float amount for the decrease in channeling time the weapon gives per item slot equipped
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
-	USkeletalMesh* WeaponMesh;
+	float ChannelingDecrease = 0.0f;
+
+	/*Functions*/
+
+	UWeaponItem();
 
 	//Function for comparing damage types againsy health variable resistances, and then applying the residual damage
 	UFUNCTION(BlueprintCallable)
-	void ApplyDamageTypes(FHitResult hitresult);
+	void ApplyDamageTypes(FHitResult HitResult);
 
 	//Function for applying the weapon's physics force to the hit actor bone
 	UFUNCTION(BlueprintCallable)
-	void ApplyPhysicsForce(FHitResult hitresult, ASPCharacter* Character);
-	
+	void ApplyPhysicsForce(FHitResult HitResult);
 
+	/*Overrides*/
 
+	//Override function for copying the item
+	//virtual USPItem* CopyItem() override;
 
-protected:
+private:
 
-	virtual void Use(class ASPCharacter* Character)override;
-	
+	//Function for retrieving the total damage incoming from the weapon
+	float CalculateTotalDamage();
 };
