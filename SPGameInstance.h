37f65009eb64940d9a37e2392e50e6cb132343d5
John <47099626+Gameartist3d@users.noreply.github.com>
2025-01-22 00:43:22 -0500

    Create SPGameInstance.h

---
+++ SPGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SPGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPV1_API USPGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
